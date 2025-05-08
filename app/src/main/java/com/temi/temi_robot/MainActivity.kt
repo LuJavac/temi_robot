4008262e2e2cef98da8bab82b02cc6cafd597032
johan <johan.petit@edu.esiee.fr>
2025-05-08 12:14:33 +0800

    appli bouton basique

--- app/src/main/java/com/temi/temi_robot/MainActivity.kt
+++ app/src/main/java/com/temi/temi_robot/MainActivity.kt
@@ -2,46 +2,25 @@ package com.temi.temi_robot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.temi.temi_robot.ui.theme.Temi_robotTheme
+
+/////////////////////////////////////////////////////////
+
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Temi_robotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+
+        startButton.setOnClickListener{
+            Toast.makeText(this, "Bouton cliqué !", Toast.LENGTH_SHORT).show()
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Temi_robotTheme {
-        Greeting("Android")
     }
+
+
 }
