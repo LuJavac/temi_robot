df7e6437fd2e8e574dd1f5d1ba4e9f21fa10e943
johan <johan.petit@edu.esiee.fr>
2025-05-09 10:43:42 +0800

    ajout dependencies et class controller

--- app/src/main/java/com/temi/temi_robot/MainActivity.kt
+++ app/src/main/java/com/temi/temi_robot/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.ComponentActivity
 import android.widget.Button
 import android.widget.Toast
 
-class MainActivity : ComponentActivity() {
+public class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/temi/temi_robot/RobotController.kt
@@ -0,0 +1,13 @@
+package com.temi.temi_robot
+
+import com.robotemi.sdk.Robot
+import android.util.Log
+import com.robotemi.sdk.SttLanguage
+import com.robotemi.sdk.TtsRequest
+import com.robotemi.sdk.constants.CliffSensorMode
+import com.robotemi.sdk.constants.HardButton
+
+
+
+class RobotController {
+}
