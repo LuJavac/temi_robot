ae6157cb87bf3e22544e3f90286a2ccf73b31144
johan <johan.petit@edu.esiee.fr>
2025-05-09 16:26:42 +0800

    dialogue commandes de base

--- app/src/main/java/com/temi/temi_robot/RobotController.kt
+++ app/src/main/java/com/temi/temi_robot/RobotController.kt
@@ -1,54 +1,37 @@
 package com.temi.temi_robot
 
-import android.util.Log
+import android.widget.Toast
+import com.robotemi.sdk.NlpResult
 import com.robotemi.sdk.Robot
 import com.robotemi.sdk.SttLanguage
 import com.robotemi.sdk.TtsRequest
-import com.robotemi.sdk.constants.CliffSensorMode
-import com.robotemi.sdk.constants.HardButton
-import com.robotemi.sdk.constants.SensitivityLevel
-import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener
-import com.robotemi.sdk.listeners.OnConversationStatusChangedListener
-import com.robotemi.sdk.listeners.OnDetectionDataChangedListener
-import com.robotemi.sdk.listeners.OnDetectionStateChangedListener
-import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener
-import com.robotemi.sdk.listeners.OnMovementStatusChangedListener
-import com.robotemi.sdk.listeners.OnRobotDragStateChangedListener
-import com.robotemi.sdk.listeners.OnRobotLiftedListener
-import com.robotemi.sdk.listeners.OnRobotReadyListener
-import com.robotemi.sdk.listeners.OnTtsVisualizerWaveFormDataChangedListener
-import com.robotemi.sdk.map.MapDataModel
-import com.robotemi.sdk.model.DetectionData
-import com.robotemi.sdk.navigation.model.Position
-import com.robotemi.sdk.navigation.model.SpeedLevel
-import kotlinx.coroutines.delay
+import com.robotemi.sdk.constants.Gender
+import com.robotemi.sdk.voice.model.TtsVoice
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+
 // Data classes
-data class WakeUp(
-    val result: String
-)
+data class AskResult(val result: String, val id: Long = System.currentTimeMillis())
 
 class RobotController():
-    Robot.WakeupWordListener
+    Robot.AsrListener
 {
     private val robot = Robot.getInstance() // Create robot object
 
     // Add listeners to robot instance
     init {
-        robot.addWakeupWordListener(this)
+        robot.addAsrListener(this)
     }
 
     // Stateflows for listeners
-    private val _wakeUp = MutableStateFlow(WakeUp("hello"))
-    val wakeUp = _wakeUp.asStateFlow()
+    private val _askResult = MutableStateFlow(AskResult("hello"))
+    val askResult = _askResult.asStateFlow()
 
 
     // General functions
-    fun speak(speech: String, haveFace: Boolean = true) {
-
+    fun speak(speech: String, haveFace: Boolean = true) { // Make the robot speak
         // Creating TTS request before speaking
         val request = TtsRequest.create(
             speech = speech,
@@ -56,24 +39,50 @@ class RobotController():
             showAnimationOnly = haveFace,
             language = TtsRequest.Language.EN_US
         )
-
         // Speaking with sdk function
         robot.speak(request)
     }
 
+    fun setTtsVoice(gender: Gender, speed: Float, pitch: Int){ // Setting voice parameters
+        // Define voice settings
+        val voiceSettings = TtsVoice(gender, speed, pitch)
+        // set the voice
+        robot.setTtsVoice(voiceSettings)
+    }
 
-    fun wakeUp() {
-        robot.wakeup(listOf(SttLanguage.SYSTEM))
+    fun askQuestion(question: String) {
+        robot.askQuestion(question)
+    }
+
+    fun finishConversation(){
+        robot.finishConversation()
     }
 
     // Overrides
-    override fun onWakeupWord(wakeupWord: String, direction: Int) {
-        _wakeUp.update {
-            WakeUp(wakeupWord)
+    override fun onAsrResult(asrResult: String, sttLanguage: SttLanguage) {
+        when {
+            asrResult.equals("Hello", ignoreCase = true) -> {
+                robot.askQuestion("Hello, I'm temi, what can I do for you?")
+            }
+            asrResult.equals("Play music", ignoreCase = true) -> {
+                robot.finishConversation()
+                speak("Okay, please enjoy.")
+            }
+            asrResult.equals("Play movie", ignoreCase = true) -> {
+                robot.finishConversation()
+                speak("Okay, please enjoy.")
+            }
+            asrResult.lowercase().contains("follow me") -> {
+                robot.finishConversation()
+                speak("Okay, please enjoy.")
+            }
+            asrResult.lowercase().contains("go to home base") -> {
+                robot.finishConversation()
+                speak("Okay, please enjoy.")
+            }
+            else -> {
+                robot.askQuestion("Sorry I can't understand you, could you please ask something else?")
+            }
         }
     }
-
-
-
-
 }
--- app/src/main/java/com/temi/temi_robot/SpeechControl.kt
+++ app/src/main/java/com/temi/temi_robot/SpeechControl.kt
@@ -3,9 +3,9 @@ package com.temi.temi_robot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-/////////////////////////////////////////////////////////
-
 import android.widget.Button
+import com.robotemi.sdk.constants.Gender
+
 
 public class SpeechControl : ComponentActivity() {
     private val robotController = RobotController()
@@ -14,10 +14,14 @@ public class SpeechControl : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // Robot voice settings : speed 0.5f-2.0f and pitch -10-10
+        robotController.setTtsVoice(Gender.FEMALE, 2.0f, 5)
+
         val startButton = findViewById<Button>(R.id.start_button)
 
         startButton.setOnClickListener{
-            robotController.speak("Hello Cecile")
+            robotController.askQuestion("How are you ?")
         }
 
     }
