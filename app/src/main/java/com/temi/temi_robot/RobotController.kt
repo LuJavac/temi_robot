296b7736dddbba386fe375ea9ace1479fc6ec7b5
johan <johan.petit@edu.esiee.fr>
2025-05-13 12:02:28 +0800

    Q&A library

--- app/src/main/java/com/temi/temi_robot/RobotController.kt
+++ app/src/main/java/com/temi/temi_robot/RobotController.kt
@@ -29,6 +29,15 @@ class RobotController():
     private val _askResult = MutableStateFlow(AskResult("hello"))
     val askResult = _askResult.asStateFlow()
 
+    // Lists for Q&A
+    private val openingHours = listOf("time", "hours", "hour", "opened", "opening", "opens", "close", "closes", "closing")
+    private val loansAndReturns = listOf("loan", "loans", "loaned", "loaning", "borrow", "borrowed", "borrowing", "return", "returns", "returning")
+    private val bookingConditions = listOf("book", "bookings", "booking", "booked", "reservation", "reservations", "reserved")
+    private val lossesCardAndStuff = listOf("lost", "loss", "losses")
+    private val databases = listOf("databases", "database")
+    private val examinationPapers = listOf("examination", "examination", "test papers", "model answers")
+    private val journalsCollection = listOf("journal", "journals", "magazine", "magazines", "newspaper", "newspapers")
+
 
     // General functions
     fun speak(speech: String, haveFace: Boolean = true) { // Make the robot speak
@@ -56,12 +65,37 @@ class RobotController():
 
     // Overrides
     override fun onAsrResult(asrResult: String, sttLanguage: SttLanguage) {
-        if(asrResult.lowercase().contains("bye")){
+
+        if(openingHours.any { word -> asrResult.contains(word, ignoreCase = true)}){ // Checks if one of the key words is part of the demand
             robot.finishConversation()
-            speak("Okay bye bye")
-        } else {
+            speak("The main library is opened from 8:30 am to 8 pm from Monday to Friday, and is closed on Saturdays. The reading lounge is opened from 8:30 am to 9 pm from Monday to Friday, and from 8:30 am to 1 pm on Saturdays. On Sundays and public holidays, everything is closed.")
+        }
+        else if(loansAndReturns.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak("You can borrow or return all accompanying materials at the Information Services Counter on Level 4. However, you cannot borrow new library materials if you haven't returned overdue items. You can renew your loans at the Self-Check Machines at the Information Services Counter on level 4, or via the internet library portal. It is all free of charges. To check your loan records, you can use the internet library portal.")
+        }
+        else if(bookingConditions.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak("To manage your bookings, please use the internet library portal. Reservations are free of charge, and can only be placed on items with status \"on loan\" and \"in processed\". Others status cannot be reserved. Reserved items must be collected by the person who booked them.")
+        }
+        else if(lossesCardAndStuff.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak("For any questions in case you've lost your library card or any borrowed item, please report to the Information Services Counter, at level 4.")
+        }
+        else if(databases.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak("To access online databases, you can connect via the internet library portal and go to \"Resources\". These are available to all NYP students and full-time staff.")
+        }
+        else if(examinationPapers.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak("To access past examination papers, you must connect via the internet library portal. Electronic copies from academic year 2015 onwards are available in PDF. You can access, download and print them. However, common test papers and model answers to examination papers are not available.")
+        }
+        else if(journalsCollection.any { word -> asrResult.contains(word, ignoreCase = true)}){
+            robot.finishConversation()
+            speak( "The journals are located at different places. The lifestyles magazines are at level 4, and the health, sciences and academic journals are at level 5.")
+        }
+        else {
             robot.startDefaultNlu(asrResult, SttLanguage.EN_US)
-            robot.askQuestion("Do you need something else ?")
         }
 
     }
